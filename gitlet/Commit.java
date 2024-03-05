4ff66403f1135e5452ada743e215c5e823f07fe3
zla218 <zla218@sfu.ca>
2024-03-04 23:17:01 -0800

    updated author of Commit.java

--- gitlet/Commit.java
+++ gitlet/Commit.java
@@ -15,7 +15,7 @@ import java.util.ArrayList;
  *  TODO: It's a good idea to give a description here of what else this Class
  *  does at a high level.
  *
- *  @author TODO
+ *  @author PepperoniBlvd
  */
 public class Commit implements Serializable{
     /**
